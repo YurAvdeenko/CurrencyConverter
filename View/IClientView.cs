fd4f642a9e22b3c12f5d9109a3d3fd0bb68f03fd
YuryAvdeenko <avdeenko.yura@mail.ru>
2021-12-30 14:13:48 +0300

    Add Client's View

---
+++ View/IClientView.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace View
+{
+    public interface IClientView : IView
+    {
+        int GetIndexOfOperation();
+        double GetSumOfOperation();
+        double GetSellCurrencyRate(int index);
+        double GetBuyCurrencyRate(int index);
+        string GetCurrencyName(int index);
+        void SetLimit(int id);
+    }
+}
